61d976434be7fb0a54b315f8e413310b176ac3ee
Carlos Marinho <carluizfla@hotmail.com>
2020-10-30 15:26:23 -0300

    adicionado as 2 rotas home e products

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+const Home = () => <div>Meu App</div>
+const Products = () => <div>Meus Produtos</div>
 
 const App = () => {
     return (
-        <div>Meu App</div>
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/products" component={Products} />
+            </Switch>
+        </BrowserRouter>
     )
 }
 
