5e7efcb8640de44345481ee90b0f8c2e4134d8ea
Carlos Marinho <carluizfla@hotmail.com>
2020-10-30 23:42:38 -0300

    conectando na api graphql

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,35 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient, InMemoryCache } from 'apollo-boost';
+import { createHttpLink } from 'apollo-link-http';
 
-const Home = () => <div>Meu App</div>
+import Home from '../pages/home';
+// const Home = () => <div>Meu App</div>
 const Products = () => <div>Meus Produtos</div>
+
+const httpLink = createHttpLink({
+    uri: 'https://api.code-challenge.ze.delivery/public/graphql'
+});
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+    link: httpLink,
+    cache
+})
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/products" component={Products} />
-            </Switch>
-        </BrowserRouter>
+        <ApolloProvider client={client}>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/products" component={Products} />
+                </Switch>
+            </BrowserRouter>
+        </ApolloProvider>
     )
 }
 
-export default App;
+export default App; 
