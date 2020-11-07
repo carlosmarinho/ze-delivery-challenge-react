4b4f5820717d0e47ead2ebc8883308572337f936
Carlos Marinho <carluizfla@hotmail.com>
2020-11-07 18:16:30 -0300

    configurado e incluido o primeiro test sem nenhuma verificação

---
+++ src/components/__tests__/app.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../App';
+import { isType } from 'graphql';
+
+let wrapped;
+
+it('shows the app component', () => {
+
+})
