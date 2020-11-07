0186b08570bf93d439f8cb40692fd190fd7e8a75
Carlos Marinho <carluizfla@hotmail.com>
2020-11-07 19:56:56 -0300

    app deve renderizar header e footer

--- src/components/__tests__/app.test.js
+++ src/components/__tests__/app.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from '../App';
+import Header from '../common/Header';
+import Footer from '../common/Footer';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() })
 
@@ -13,8 +15,11 @@ const findByTestAttr = (wrapper, val) => {
     return wrapper.find(`[data-test="${val}"]`)
 }
 
-it('shows the app component without error', () => {
+it('Should render a Footer and a Header', () => {
     const wrapper = setup();
-    const appComponent = findByTestAttr(wrapper, 'component-app');
-    expect(appComponent.length).toBe(1);
+    const header = wrapper.find(Header);
+    const footer = wrapper.find(Footer);
+    expect(header.length).toBe(1);
+    expect(footer.length).toBe(1);
 })
+// const appComponent = findByTestAttr(wrapper, 'component-app');
