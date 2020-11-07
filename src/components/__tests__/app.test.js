bde5e09048bb7c61c9b78d2bb8c1ef8f670f5df7
Carlos Marinho <carluizfla@hotmail.com>
2020-11-07 19:06:14 -0300

    Deve dar error ao exibir pois ainda não foi aplicado no app

--- src/components/__tests__/app.test.js
+++ src/components/__tests__/app.test.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from '../App';
-import { isType } from 'graphql';
 
-let wrapped;
+Enzyme.configure({ adapter: new EnzymeAdapter() })
 
-it('shows the app component', () => {
+const setup = (props={}) => {
+    return shallow(<App {...props} />);
+}
 
+const findByTestAttr = (wrapper, val) => {
+    return wrapper.find(`[data-test="${val}"]`)
+}
+
+it('shows the app component without error', () => {
+    const wrapper = setup();
+    const appComponent = findByTestAttr(wrapper, 'component-app');
+    expect(appComponent.length).toBe(1);
 })
